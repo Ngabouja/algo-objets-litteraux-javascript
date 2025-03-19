f19d963bc2a13eef56db198b86e64686e106da6f
Ngabouja <ngabouja@edenschool.fr>
2025-03-19 10:16:52 +0100

    feat(ex): ajout ex03

---
+++ ex01.js
@@ -0,0 +1,5 @@
+function afficherInfosPersonne(personne) {
+	return personne;
+}
+
+console.log(afficherInfosPersonne({ nom: "Alice", age: 30, ville: "Paris" })); // 'Alice a 30 ans et habite à Paris.'
---
+++ ex03.js
@@ -0,0 +1,6 @@
+function estMajeur(personne) {
+	
+}
+
+console.log(estMajeur({ nom: "Alice", age: 30 })); // true
+console.log(estMajeur({ nom: "Bob", age: 15 })); // false
